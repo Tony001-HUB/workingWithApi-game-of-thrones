b1f1b404261d1f7b9d1e5ba4734d5ab2ca2ccec8
Anton <Osipenko>
2021-02-16 19:39:45 +0300

    getting the random character API 1.1

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,29 +1,51 @@
 import React, {Component} from 'react';
+import gotService from '../../services/gotService'
 import './randomChar.css';
 
 export default class RandomChar extends Component {
 
+    constructor(){
+        super();
+        this.updateCharacter();
+    }
+
+    gotService = new gotService();
+    state = {
+        character: {}
+    }
+
+    onCharacterLoaded = (character) =>{
+        this.setState({character})
+    }
+
+    updateCharacter(){      
+        const id = Math.floor(Math.random()*140 + 25);
+        this.gotService.getCharacter(id)
+            .then(this.onCharacterLoaded);
+    }
+
     render() {
+        const {character: {name, gender, born, died, culture}} = this.state
 
         return (
             <div className="random-block rounded">
-                <h4>Random Character: John</h4>
+                <h4>Random Character: {name}</h4>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Gender </span>
-                        <span>male</span>
+                        <span>{gender}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Born </span>
-                        <span>11.03.1039</span>
+                        <span>{born}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Died </span>
-                        <span>13.09.1089</span>
+                        <span>{died}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Culture </span>
-                        <span>Anarchy</span>
+                        <span>{culture}</span>
                     </li>
                 </ul>
             </div>
---
+++ src/services/gotService.js
@@ -0,0 +1,80 @@
+class GotService{
+    constructor(){
+        this._apiBase = 'https://www.anapioficeandfire.com/api';
+    }
+
+        async getResurse(url){
+        const res = await fetch(`${this._apiBase}${url}`);
+    
+        if(!res.ok){
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
+        return await res.json();
+    };
+
+    async getAllCharacters(){
+        const res = await this.getResurse('/characters?page=5&pageSize=10');
+        return res.map(this._transformCharacter);
+    }
+
+    async getCharacter(id){
+        const character = await this.getResurse(`/characters/${id}`);
+        return (this._transformCharacter(character));
+    }
+
+    async getAllHouses(){
+        const res = await this.getResurse(`/houses/`);
+        return res.map(this._transformCharacter);
+    }
+
+    async getHouses(id){
+        const hous = await this.getResurse(`/houses/${id}/`);
+        this._transformCharacter(hous);
+    }
+
+    async getAllBooks(){
+        const res = await this.getResurse(`/books/`);
+        return res.map(this._transformCharacter);
+    }
+
+    async getBooks(id){
+        const book = await this.getResurse(`/books/${id}/`);
+        this._transformCharacter(book);
+    }
+
+    _transformCharacter(character){
+        return {
+            name: character.name,
+            gender: character.gender,
+            born: character.born,
+            died: character.died,
+            culture: character.culture
+        }
+    }
+
+    _transformHouse(house){
+        return{
+            name: house.name,
+            region: house.region,
+            words: house.words,
+            titles: house.titles,
+            overlord: house.overlord,
+            ancestralWeapons: house.ancestralWeapons
+
+        }
+    }
+
+    _transformBook(book){
+        return{
+            name: book.name,
+            numberOfPages: book.numberOfPages,
+            publiser: book.publiser,
+            released: book.released
+        }
+    }
+
+
+}
+
+export default GotService;
